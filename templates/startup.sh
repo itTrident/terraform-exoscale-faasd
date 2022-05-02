db4d05cd11fbd51bc94b150e3ec78d918962477c
r4jeshwar <100279715+r4jeshwar@users.noreply.github.com>
2022-05-02 15:19:10 +0530

    Update startup.sh

--- templates/startup.sh
+++ templates/startup.sh
@@ -4,7 +4,7 @@ mkdir -p /var/lib/faasd/secrets/
 echo ${basic_auth_user} > /var/lib/faasd/secrets/basic-auth-user
 echo ${basic_auth_password} > /var/lib/faasd/secrets/basic-auth-password
 
-export FAASD_DOMAIN="${email}"
-export LETSENCRYPT_EMAIL="${domain}"
+export LETSENCRYPT_EMAIL="${email}"
+export FAASD_DOMAIN="${domain}"
 
 bash <(curl -sfL https://raw.githubusercontent.com/openfaas/faasd/master/hack/install.sh)
